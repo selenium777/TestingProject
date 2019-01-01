a6d46a7af6b76cc8e9b885bce8f8eab483403c9b
selenium777 <111ankitsingh@gmail.com>
2019-01-01 20:38:14 +0530

    First Commit

---
+++ BasicMavenProject/src/basic1/HelloWorld.java
@@ -0,0 +1,12 @@
+package basic1;
+
+import org.testng.annotations.Test;
+
+public class HelloWorld {
+
+	@Test
+	public void basicConfig()
+	{
+		
+	}
+}
---
+++ BasicMavenProject/src/demo/Log4jDemo.java
@@ -0,0 +1,19 @@
+package demo;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Log4jDemo {
+
+	static Logger logger = LogManager.getLogger(Log4jDemo.class);
+	
+	public static void main(String[] args) {
+
+		logger.debug("2 This is debug message...");
+		logger.error("1 error message...");
+		logger.info("1 info message....");
+		logger.warn("1 warn message...");
+		logger.traceEntry("1 trace message...");
+	}
+
+}
